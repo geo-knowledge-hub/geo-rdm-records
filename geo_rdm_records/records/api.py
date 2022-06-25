1f0d3e4a944ffc792f83422cf001b7d5763d8c14
Felipe Menino <efelipecarlos@gmail.com>
2022-06-25 13:28:23 -0300

    record: updating Record API (Closes #11)

--- geo_rdm_records/records/api.py
+++ geo_rdm_records/records/api.py
@@ -21,6 +21,8 @@ from invenio_records_resources.records.systemfields import (
     PIDRelation,
 )
 from invenio_vocabularies.contrib.affiliations.api import Affiliation
+from invenio_vocabularies.contrib.awards.api import Award
+from invenio_vocabularies.contrib.funders.api import Funder
 from invenio_vocabularies.contrib.subjects.api import Subject
 from invenio_vocabularies.records.api import Vocabulary
 
@@ -52,51 +54,66 @@ class CommonFieldsMixin(BaseCommonFieldsMixin):
             pid_field=Vocabulary.pid.with_type_ctx("engagementprioritiestypes"),
             cache_key="engagement_priorities",
         ),
+
         #
         # InvenioRDM DataCite Relations
         #
         creator_affiliations=PIDNestedListRelation(
             "metadata.creators",
             relation_field="affiliations",
-            attrs=["id", "name"],
+            keys=["name"],
             pid_field=Affiliation.pid,
             cache_key="affiliations",
         ),
         contributor_affiliations=PIDNestedListRelation(
             "metadata.contributors",
             relation_field="affiliations",
-            attrs=["id", "name"],
+            keys=["name"],
             pid_field=Affiliation.pid,
             cache_key="affiliations",
         ),
+        funding_funder=PIDListRelation(
+            "metadata.funding",
+            relation_field="funder",
+            keys=["name"],
+            pid_field=Funder.pid,
+            cache_key="funders",
+        ),
+        funding_award=PIDListRelation(
+            "metadata.funding",
+            relation_field="award",
+            keys=["title", "number", "identifiers"],
+            pid_field=Award.pid,
+            cache_key="awards",
+        ),
         languages=PIDListRelation(
             "metadata.languages",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("languages"),
             cache_key="languages",
         ),
         resource_type=PIDRelation(
             "metadata.resource_type",
-            attrs=["id", "title", "props.type", "props.subtype"],
+            keys=["title", "props.type", "props.subtype"],
             pid_field=Vocabulary.pid.with_type_ctx("resourcetypes"),
             cache_key="resource_type",
             value_check=dict(tags=["depositable"]),
         ),
         subjects=PIDListRelation(
             "metadata.subjects",
-            attrs=["id", "subject", "scheme"],
+            keys=["subject", "scheme"],
             pid_field=Subject.pid,
             cache_key="subjects",
         ),
         licenses=PIDListRelation(
             "metadata.rights",
-            attrs=["id", "title", "description", "icon", "props.url", "props.scheme"],
+            keys=["title", "description", "icon", "props.url", "props.scheme"],
             pid_field=Vocabulary.pid.with_type_ctx("licenses"),
             cache_key="licenses",
         ),
         related_identifiers=PIDListRelation(
             "metadata.related_identifiers",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("resourcetypes"),
             cache_key="resource_type",
             relation_field="resource_type",
@@ -104,56 +121,56 @@ class CommonFieldsMixin(BaseCommonFieldsMixin):
         ),
         title_types=PIDListRelation(
             "metadata.additional_titles",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("titletypes"),
             cache_key="title_type",
             relation_field="type",
         ),
         title_languages=PIDListRelation(
             "metadata.additional_titles",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("languages"),
             cache_key="languages",
             relation_field="lang",
         ),
         creators_role=PIDListRelation(
             "metadata.creators",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("creatorsroles"),
             cache_key="role",
             relation_field="role",
         ),
         contributors_role=PIDListRelation(
             "metadata.contributors",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("contributorsroles"),
             cache_key="role",
             relation_field="role",
         ),
         description_type=PIDListRelation(
             "metadata.additional_descriptions",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("descriptiontypes"),
             cache_key="description_type",
             relation_field="type",
         ),
         description_languages=PIDListRelation(
             "metadata.additional_descriptions",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("languages"),
             cache_key="languages",
             relation_field="lang",
         ),
         date_types=PIDListRelation(
             "metadata.dates",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("datetypes"),
             cache_key="date_types",
             relation_field="type",
         ),
         relation_types=PIDListRelation(
             "metadata.related_identifiers",
-            attrs=["id", "title"],
+            keys=["title"],
             pid_field=Vocabulary.pid.with_type_ctx("relationtypes"),
             cache_key="relation_types",
             relation_field="relation_type",
@@ -177,7 +194,7 @@ class GEODraft(CommonFieldsMixin, Draft):
     model_cls = rdm_models.RDMDraftMetadata
 
     index = IndexField(
-        "geordmrecords-drafts-draft-v4.0.0", search_alias="geordmrecords"
+        "geordmrecords-drafts-draft-v5.0.0", search_alias="geordmrecords"
     )
 
     files = FilesField(
@@ -206,7 +223,7 @@ class GEORecord(CommonFieldsMixin, Record):
     model_cls = rdm_models.RDMRecordMetadata
 
     index = IndexField(
-        "geordmrecords-records-record-v4.0.0", search_alias="geordmrecords-records"
+        "geordmrecords-records-record-v5.0.0", search_alias="geordmrecords-records"
     )
 
     files = FilesField(
