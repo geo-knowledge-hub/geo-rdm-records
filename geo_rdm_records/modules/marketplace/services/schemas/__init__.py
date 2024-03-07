4f48a0f087a795f0895ca0d7f9dfb1b446fdef0f
Felipe Carlos <efelipecarlos@gmail.com>
2024-03-07 06:03:50 -0300

    marketplace: adding extra metadata for marketplace items

---
+++ geo_rdm_records/modules/marketplace/services/schemas/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2024 GEO Secretariat.
+#
+# geo-rdm-records is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""GEO Marketplace Services schemas."""
+
+from marshmallow import Schema, fields, validate
+from marshmallow_utils.fields import NestedAttribute, SanitizedUnicode
+
+from geo_rdm_records.base.services.schemas import MetadataSchema as BaseMetadataSchema
+from geo_rdm_records.base.services.schemas.records import BaseGEORecordSchema
+
+
+#
+# Metadata
+#
+class MarketplaceFields(Schema):
+    """Fields for marketplace object."""
+
+    launch_url = SanitizedUnicode(required=True, validate=validate.URL())
+
+    vendor_contact = SanitizedUnicode(required=True, validate=validate.Email())
+
+
+class MetadataSchema(BaseMetadataSchema):
+    """Metadata schema for marketplace items."""
+
+    marketplace = fields.Nested(MarketplaceFields)
+
+
+#
+# Marketplace item
+#
+class GEOMarketplaceItemSchema(BaseGEORecordSchema):
+    """Marketplace item metadata."""
+
+    metadata = NestedAttribute(MetadataSchema)
