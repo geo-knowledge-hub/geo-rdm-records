c07daa1b553c997ff1183a85794a5df4740df764
Felipe Carlos <efelipecarlos@gmail.com>
2024-04-10 13:14:43 -0300

    search: adding support for expandable fields

--- geo_rdm_records/base/resources/config.py
+++ geo_rdm_records/base/resources/config.py
@@ -7,14 +7,22 @@
 
 """Base resources configuration."""
 
+from flask_resources import ResponseHandler
 from invenio_rdm_records.resources.config import (
     RDMRecordResourceConfig as BaseRecordResourceConfig,
 )
 
 from geo_rdm_records.base.resources.args import GEOSearchRequestArgsSchema
+from geo_rdm_records.base.resources.serializers import UIRecordJSONSerializer
 
 
 class BaseGEOResourceConfig(BaseRecordResourceConfig):
     """Record resource configuration."""
 
     request_search_args = GEOSearchRequestArgsSchema
+
+    # Resource routes
+    response_handlers = {
+        **BaseRecordResourceConfig.response_handlers,
+        "application/vnd.inveniordm.v1+json": ResponseHandler(UIRecordJSONSerializer()),
+    }
